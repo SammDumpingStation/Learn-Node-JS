a3cd97235467a48de1471d3f93cae869b403d475
Samm Caagbay <sammcaag@gmail.com>
2024-03-31 10:29:30 +0800

    👍 Using app.js to change raw Node into Express

--- app.js
+++ app.js
@@ -1,68 +1,21 @@
-const arrayFromLogs = require("./log");
+const express = require("express");
 
-const os = require("os");
-// console.log(__dirname);
-// console.log(__filename);
+// express app
+const app = express();
 
-// console.log(os.platform(), os.homedir());
+//listen for request
+app.listen(3000);
 
-//reading Files
-
-const fileSystem = require("fs");
-
-// fileSystem.readFile('./log.js', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString());
-// });
-
-//writing files
-
-// fileSystem.writeFile('./docsExample/text.txt', 'Ayooo Whazzzuuppp', () => {
-//   console.log('the file was written');
-// });
-
-// fileSystem.writeFile("./docsExample/niggaballs.txt", "Ayooo Ninjasszzz", () => {
-//   console.log("a new file was written");
-// });
-
-//create directories
-
-// if (!fileSystem.existsSync('./the-made-directory-sir')) {
-//  fileSystem.mkdir("./the-made-directory-sir", (err) => {
-//    if (err) {
-//      console.log(err);
-//    }
-//    console.log("folder was created");
-//  });
-// } else {
-//   fileSystem.rmdir('./the-made-directory-sir', (err)=> {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log('The directory was deleted!');
-//   })
-// };
-
-//creating read stream
-
-const readStream = fileSystem.createReadStream('./docsExample/largeAmountsOfData.txt', {encoding: 'utf-8'});
-const writeStream = fileSystem.createWriteStream('./docsExample/secondCuming.txt');
-
-// readStream.on('data', (chunk) => {
-//   console.log("------------- New Buffer -------------");
-//   console.log(chunk);
-//   writeStream.write('\n New Chunk \n');
-//   writeStream.write(chunk);
-// })
-
-//piping
-// readStream.pipe(writeStream);
-
-fileSystem.rmdir('./docsExample', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('The directory was deleted');
+app.get("/", (request, response) => {
+  response.sendFile('./HTML-CSS-Practice/01-text-styles.html', { root: __dirname})
 });
+
+app.get("/amazon", (request, response) => {
+  response.sendFile("./HTML-CSS-Practice/02-amazon-button-practice.html", {
+    root: __dirname,
+  });
+});
+
+app.get('/company', (request, response) => {
+  response.sendFile('./HTML-CSS-Practice/03-buttons-of-company-practice.html', {root: __dirname});
+})

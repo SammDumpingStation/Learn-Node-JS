43b16d691cad589c7ecb09bb8cb7b59828d84df6
Samm Caagbay <sammcaag@gmail.com>
2024-03-31 09:10:27 +0800

    🔢 Adding HTML and using switch statements to switch different url's

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const http = require("http");
-const fs = require("fs");
+const fileSystem = require("fs");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+const server = http.createServer((request, result) => {
+  let htmlFile = "./HTML-CSS-Practice/";
+  result.setHeader("Content-Type", "text/html/css");
 
-  res.setHeader("Content-Type", "text/html");
+  switch (request.url) {
+    case "/":
+      htmlFile += "01-text-styles.html";
+      break;
+    case "/amazon":
+      htmlFile += "02-amazon-button-practice.html";
+      break;
+    case "/company":
+      htmlFile += "03-buttons-of-company-practice.html";
+      break;
+    case "/padding":
+      htmlFile += "04-padding-margin-practice.html";
+      break;
+    default:
+      htmlFile += "05-404.html";
+      break;
+  }
 
-  fs.readFile("./HTML-Files/index.html", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
+  fileSystem.readFile(htmlFile, (error, data) => {
+    if (error) {
+      console.log(error);
     } else {
-      res.write(data);
-      res.end();
+      result.end(data);
     }
   });
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("server was requested");
+  console.log('listening on server 3000');
 });

d5261fc177c970abafb9861f2c32d024b645a6a4
Samm Caagbay <sammcaag@gmail.com>
2024-03-31 08:31:24 +0800

    ⚗️ Reading and Writing HTML Files and integrate it into Node Local Server

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
   res.setHeader("Content-Type", "text/html");
-  res.write("<p>Hello Niggas</p>");
-  res.write("<button>Click Me!</button>");
-  res.end();
+
+  fs.readFile("./HTML-Files/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
